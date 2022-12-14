5c83a5f73c27e32d62c95267cf315bf68cb07835
JOJO-CPU <1797175504@qq.com>
2022-12-14 18:21:27 +0800

    vue3.2项目通知栏修改

--- vue3.2+vite+pinia+typescript+element-plus+vue-router/src/layout/components/Header/components/Remind.vue
+++ vue3.2+vite+pinia+typescript+element-plus+vue-router/src/layout/components/Header/components/Remind.vue
@@ -10,15 +10,11 @@
         <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
           <el-tab-pane label="通知" name="first">
             <div class="item-child">
-              GitHub开源地址：<el-button type="primary" link @click="toGitHub('https://github.com/zouzhibin/vue-admin-perfect')">点我</el-button></div
+              GitHub开源地址：<el-button type="primary" link @click="toGitHub('https://github.com/JOJO-CPU/YueShan-Vue-templete')">点我</el-button></div
             >
             <el-divider style="margin-bottom: 15px"/>
             <div class="item-child">
-              Gitee开源地址：<el-button type="primary" link @click="toGitHub('https://gitee.com/yuanzbz/vue-admin-perfect')">点我</el-button></div
-            >
-            <el-divider />
-            <div class="item-child">
-              github开源地址：<el-button type="primary" link @click="toGitHub('https://github.com/zouzhibin/vue-admin-perfect')">点我</el-button></div
+              Gitee开源地址：<el-button type="primary" link @click="toGitHub('https://github.com/JOJO-CPU/YueShan-Vue-templete')">点我</el-button></div
             >
           </el-tab-pane>
         </el-tabs>
